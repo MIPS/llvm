c616964feac07b89b43530f15e4a74e89458bec6
Jovan Dmitrović <jovan.dmitrovic@syrmia.com>
2023-07-27 15:07:12 +0200

    Added {-m, -mno}-load-store-bonding options

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -2158,12 +2158,16 @@ void Clang::AddRISCVTargetArgs(const ArgList &Args,
   }
 
   if (Arg *A = Args.getLastArg(options::OPT_mload_store_pairs,
-                               options::OPT_mno_load_store_pairs)) {
-    if (A->getOption().matches(options::OPT_mload_store_pairs)) {
+                               options::OPT_mno_load_store_pairs,
+                               options::OPT_mload_store_bonding,
+                               options::OPT_mno_load_store_bonding)) {
+    if (A->getOption().matches(options::OPT_mload_store_pairs) ||
+        A->getOption().matches(options::OPT_mload_store_bonding)) {
       CmdArgs.push_back("-mllvm");
       CmdArgs.push_back("-riscv-load-store-pairs=1");
     }
-    if (A->getOption().matches(options::OPT_mno_load_store_pairs)) {
+    if (A->getOption().matches(options::OPT_mno_load_store_pairs) ||
+        A->getOption().matches(options::OPT_mno_load_store_bonding)) {
       CmdArgs.push_back("-mllvm");
       CmdArgs.push_back("-riscv-load-store-pairs=0");
     }
