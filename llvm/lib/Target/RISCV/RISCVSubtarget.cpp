48e08482cb98704842eff902560bfa1be697e2bc
Djordje Todorovic <djordje.todorovic@htecgroup.com>
2024-10-11 14:27:35 +0200

    [RISCV] Add MIPS extensions
    Introduce another MIPS extensions.
    Add support for:
    1) ccmov
    2) load/store pairs
    
    Allow disabling ccmov with -mno-ccmov.
    Make -mload-store-pairs imply l[w|d]p and s[w|d]p.
    
    co-authored by Dragan Mladjenovic <Dragan.Mladjenovic@syrmia.com>
    co-authored by Jovan Dmitrović <jovan.dmitrovic@htecgroup.com>

--- llvm/lib/Target/RISCV/RISCVSubtarget.cpp
+++ llvm/lib/Target/RISCV/RISCVSubtarget.cpp
@@ -61,6 +61,16 @@ static cl::opt<unsigned> RISCVMinimumJumpTableEntries(
     "riscv-min-jump-table-entries", cl::Hidden,
     cl::desc("Set minimum number of entries to use a jump table on RISCV"));
 
+static cl::opt<bool> UseLoadStorePairsOpt(
+    "riscv-load-store-pairs",
+    cl::desc("RISCV: Optimize for load-store bonding"),
+    cl::init(false), cl::Hidden);
+
+static cl::opt<bool> UseCCMovInsn(
+    "riscv-ccmov",
+    cl::desc("RISCV: Use 'ccmov' instruction"),
+    cl::init(true), cl::Hidden);
+
 void RISCVSubtarget::anchor() {}
 
 RISCVSubtarget &
@@ -198,4 +208,12 @@ unsigned RISCVSubtarget::getMinimumJumpTableEntries() const {
   return RISCVMinimumJumpTableEntries.getNumOccurrences() > 0
              ? RISCVMinimumJumpTableEntries
              : TuneInfo->MinimumJumpTableEntries;
+}
+
+bool RISCVSubtarget::useLoadStorePairs() const {
+  return UseLoadStorePairsOpt && HasLoadStorePairs;
+}
+
+bool RISCVSubtarget::useCCMovInsn() const {
+  return UseCCMovInsn && HasCCMov;
 }
